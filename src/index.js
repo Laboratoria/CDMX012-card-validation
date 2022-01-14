60f7631102170c2b697451f97e7ee3c31d12c824
Betsy Ramos <aby_7710@hotmail.com>
2022-01-13 19:55:05 -0700

    add function to encrypt numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import validator from './validator.js';
 
-console.log(validator);
+ var numbers = [];
+ 
+ function encryptText(){
+    var creditCardInput = document.getElementById("creditCardInput");
+    var size = creditCardInput.value.length;
+    var lastValue = creditCardInput.value.charAt(size-1);
+    numbers.push(lastValue);
+    creditCardInput.value = creditCardInput.value.replace(/\d/, "*");
+    console.log(numbers);
+}
