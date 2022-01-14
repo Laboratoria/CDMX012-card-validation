2873121af0ed81f9bfc25ae6f56b662468aea9e1
Betsy Ramos <aby_7710@hotmail.com>
2022-01-14 11:50:41 -0700

    function encryptText

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@
     creditCardInput.value = creditCardInput.value.replace(/\d/, "*");
     console.log(numbers);
 }
+
+function validateCardNumber(){
+    //validar 16 digitos de largo
+    //sumar numeros de pocisiones pares
+    //ciclo  mientras haya numeros de dos digitos sumar ambos números
+    //sumar todos los numeros del arreglo
+    //revisar si termina en 0
+    
+}
