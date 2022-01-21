69be51609e23b1e2f233b8fbaa352a1853f523a1
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-01-20 22:16:24 -0600

    mostrar validador de tarjeta en html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+document.getElementById("botonTarjeta").addEventListener("click", function () {
+    const inputTarjeta = document.getElementById("inputTarjeta");
+    console.log(inputTarjeta.value);
+    let resultado = validator.isValid(inputTarjeta.value)
+    if (resultado=== true ){resultado ="Tarjeta Valida"}
+    else {resultado="Tarjeta Invalida"}
+
+    document.getElementById ("resultadoVaidacion").innerHTML=resultado;
+
+   //let resultadoValidacion= document.getElementById ("resultadoVaidacion");
+  // resultadoValidacion.innerHTML=resultado;
+
+
+});
