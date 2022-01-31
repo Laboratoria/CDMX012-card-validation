c9372e57328786462963cf419df363b825ddfd35
BetsyRms <aby_7710@hotmail.com>
2022-01-31 16:50:38 -0700

    cambio a validator

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
+var numbers = [];
 
- var numbers = [];
- 
- function encryptText(){
-    var creditCardInput = document.getElementById("creditCardInput");
-    var size = creditCardInput.value.length;
-    var lastValue = creditCardInput.value.charAt(size-1);
-    numbers.push(lastValue);
-    creditCardInput.value = creditCardInput.value.replace(/\d/, "*");
-    console.log(numbers);
-}
+//var creditCardInput = document.getElementById("creditCardInput").value;
+//console.log(creditCardInput);
+//maskify: (creditCardInput)=>{
+//    console.log(creditCardInput);
+  //  var size = creditCardInput.value.length;
+    //var lastValue = creditCardInput.value.charAt(size-1);
+    //if (!size) return;
+    //numbers.push(lastValue);
+    //if(numbers.length < 13){
+    //creditCardInput.value = creditCardInput.value.replace(/\d/, "*");
+    //}
+//},
 
-function validateCardNumber(){
-    //validar 16 digitos de largo
-    //sumar numeros de pocisiones pares
-    //ciclo  mientras haya numeros de dos digitos sumar ambos números
-    //sumar todos los numeros del arreglo
-    //revisar si termina en 0
-    
+function payWithCard(){
+    document.getElementById("main").style.display = "block";
+  };
+
+  function validateLong(){
+    if(numbers.length !== /[0-9]/){
+        alert("escriba solo números")
+    }
+    if(numbers.length == 0 || /\s/.test(numbers)){
+        alert("campo vacio o con espacios")
+    }
+    if(numbers.length >= 13 && numbers.length <= 16){
+        validator.isValid(numbers);
+    }
+        else {
+        alert("ingrse un numero valido")
+        }
 }
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
+console.log("loquesea")
+var numbers = [];
 const validator = {
-  // ...
-};
+    maskify: (value)=>{
+        console.log(creditCardInput);
+        var size = value.length;
+        var lastValue = value.charAt(size-1);
+        if (!size) return;
+        numbers.push(lastValue);
+        //if(numbers.length < 13){
+        document.getElementById("creditCardInput").value = value.replace(/\d/, "*");
+        //}
+    },
+    isValid: (creditCardInput)=>{   
+        numbers.reverse()
+      for (var i=0; i<numbers.length; i++){
+          if(i%2!==0){
+              numbers[i] = numbers[i]*2;
+          }
+          else{
+              numbers[i] = Number.parseInt(numbers[i]);
+          }
+      }
+      for(var i=0; i<16; i++){
+          if(numbers[i]>9){
+             var stringArray = numbers[i].toString().split("")
+             var array1 = Number.parseInt(stringArray[0]);
+             var array2 = Number.parseInt(stringArray[1]);
+             var sum = array1 + array2
+             numbers[i] = sum
+          }
+      }
+      var total = numbers.reduce((a, b) => a + b, 0);
+      if(total%10===0){
+          alert("tarjeta valida")
+      }
+      else{
+          alert("tarjeta no valida")
+      }
+      console.log(total);
+    }
+}
 
 export default validator;
