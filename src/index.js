9e11db216c48808f702bb09ca40cd914e7fd4083
DenisseMolina <dmolinaruelas@gmail.com>
2022-02-04 13:21:18 -0600

    add files project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const button = document.getElementById("boton");
+
+button.addEventListener("click", getNumber);
+
+function getNumber() {
+  let box = document.getElementById("cardNumber").value;
+  //console.log(box);
+
+  let maskify = validator.maskify(box);
+
+  let alert = document.getElementById("validation");
+
+  let validation = validator.isValid(box);
+
+  if (validation === true) {
+    alert.innerHTML = "Tarjeta válida";
+    alert.classList.add("valid");
+    document.getElementById("maskify").innerHTML = maskify;
+  } else {
+    alert.innerHTML = "Tarjeta inválida";
+    alert.classList.add("invalid");
+    document.getElementById("maskify").innerHTML = maskify;
+  }
+
+  document.getElementById("cardNumber").innerHTML = validator.maskify(box);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,66 @@
+//Validando los números ingresados y regresandolos en reversa.
 const validator = {
-  // ...
+  isValid: function isValid(cardNumber) {
+    let parseCard = cardNumber.split("");
+    //console.log(parseCard)
+    parseCard.reverse();
+    let card = parseCard;
+    //let card = Array.from(cardNumber).reverse();
+    //console.log(card)
+
+    let result = false;
+    const card2 = []; //arreglo vacio y auxiliar
+    //console.log(cardNumber, card);
+
+    let total = 0;
+    //Recorrer el array con bucle for
+    for (let i = 0; i <= cardNumber.length - 1; i++) {
+      card[i] = parseInt(card[i]);
+      //console.log("for"+i);
+      //identificar pares y nones
+      if (i % 2 !== 0) {
+        card[i] = card[i] * 2;
+        if (card[i] >= 10) {
+          card2.push(card[i] - 9);
+        } else {
+          card2.push(card[i]);
+        }
+        //console.log(card)
+      } else {
+        card2.push(parseInt(card[i]));
+        //console.log('par '+card[i])
+      }
+      //console.log(card2);
+
+      total = total + card2[i];
+      //console.log(total);
+    }
+
+    if (total % 10 === 0) {
+      result = true;
+    } else {
+      result = false;
+    }
+    //console.log(result);
+    return result;
+  },
+
+  maskify: function maskify(cardNumber) {
+    let array = cardNumber.split("");
+    let result;
+
+    const array2 = [];
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i < cardNumber.length - 4) {
+        array2.push("#");
+      } else {
+        array2.push(array[i]);
+      }
+    }
+    result = array2.join("");
+    //console.log(result);
+    return result;
+  },
 };
 
 export default validator;
