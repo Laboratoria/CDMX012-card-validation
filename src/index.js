929c3f6727d24f7ffd83613517e6c99507d04677
BetsyRms <aby_7710@hotmail.com>
2022-02-06 19:43:32 -0700

    Vvrsión final

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import validator from "./validator.js";
 function payWithCard(){
-    document.getElementById("main").style.display = "block";
+    document.getElementById("pay").style.display = "block";
 }
   function validateLong(){
     var creditCardInput = document.getElementById("creditCardInput").value.replace(/ /g, "");
     if(creditCardInput.length >= 13 && creditCardInput.length <= 16){
         var result = validator.isValid(creditCardInput);
-        var replace1 = validator.maskify(creditCardInput);
-        document.getElementById("creditCardInput").value = replace1
-        //console.log(replace1);
         if(result === true){
+            var replace1 = validator.maskify(creditCardInput);
+            document.getElementById("creditCardInput").value = replace1
             alert("¡Gracias por tu compra!")
         } else {
             alert("Tarjeta invalida")
         }
     } else {
-        alert("ingrese un numero valido")
+        alert("Ingrese un número válido")
     }
 }
 document.getElementById("Comprar").addEventListener("click",validateLong);
